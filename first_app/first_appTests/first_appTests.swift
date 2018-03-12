02dde686fdabe94650c593d063da102b24e45d10
Adrixop95 <adrixop95@me.com>
2018-03-12 12:23:02 +0100

    init

---
+++ first_app/first_appTests/first_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  first_appTests.swift
+//  first_appTests
+//
+//  Created by Adrian Rupala on 12.03.2018.
+//  Copyright © 2018 Adrian Rupala. All rights reserved.
+//
+
+import XCTest
+@testable import first_app
+
+class first_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
